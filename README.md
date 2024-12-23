acda0c27a0da803f59f082ce5458d26ff5f04657
Plavak Das <plavakdas@gmail.com>
2024-12-23 16:41:23 +0530

    setup mongodb

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Micro-Blog
 
+## Tech Stack
+
+- Python Flask
+- HTML and CSS
+- Mongo DB
+
+# Retrieving and inserting data from/into Mongo DB
+
+- pymongo is used to connect with MongoDB client
+- Replace the MongoDB clientstring with your own mongodb string
+- In case if the password contains special characters (like '@' etc), use a parser
+
+-------
+
 Inspired from : Jose Salvatierra's course on Web Dev with Flask, Python, HTML, CSS, MongoDB
--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import datetime
+import urllib
 from flask import Flask, render_template, request
+from pymongo import MongoClient
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    mockclient_str = "<your-mongodb-string>"
+    client = MongoClient(mockclient_str)
+    app.db = client.microblog #name of database. Here it is microblog
+    entries = []
 
-@app.route("/", methods=["GET","POST"])
-def home():
-    if request.method == "POST":
-        entry_content = request.form.get("content")
-        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        print(entry_content, formatted_date)
-    return render_template("home.html")
+    @app.route("/", methods=["GET","POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            entries.append((entry_content, formatted_date))
+            app.db.entries.insert_one({"content": entry_content, "date":formatted_date})
+            
+        entries_with_date = [
+            (entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"],"%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]    
+        return render_template("home.html", entries=entries_with_date)
+    
+    return app
